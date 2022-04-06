44671aa707b460aacc49eb7b0c5890e288d31660
el mtili <mohamed.elmtili@auguria.fr>
2022-04-06 09:34:23 +0200

    [ADD] auguria_custom

---
+++ auguria_custom/__manifest__.py
@@ -0,0 +1,31 @@
+# # -*- coding: utf-8 -*-
+# # Copyright (C) 2021 - Auguria (<https://www.auguria.fr>).
+# # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# {
+#     'name': 'Auguria Demos custum',
+#     'version': '2.0',
+#     'author': 'Auguria SAS',
+#     'license': 'LGPL-3',
+#     'summary': 'Auguria demos Custom',
+#     'sequence': 15,
+#     'description': """
+# custom dev module
+#     """,
+#     'category': '',
+#     'website': 'https://www.auguria.fr/',
+#     'images': [],
+#     'depends': [
+#                 'account', 'sale', 'base', 'base_iban', 'website'
+#         ],
+#     'data': [
+#              'views/helpdesk_view.xml',
+#              'views/website_view.xml',
+#     ],
+#     'demo': [
+#     ],
+#     'qweb': [
+#     ],
+#     'installable': True,
+#     'application': True,
+#     'auto_install': False,
+# }
---
+++ auguria_custom/models/website.py
@@ -0,0 +1,13 @@
+# # -*- coding: utf-8 -*-
+# # Copyright (C) 2021 - Auguria (<https://www.auguria.fr>).
+# # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+#
+# from odoo import api, fields, models
+#
+#
+# class HelpdeskTicket(models.Model):
+#     _inherit = "website"
+#
+#     cart_header = fields.Boolean(string='Ne pas afficher le panier', help="field to active the cart in the website")
+#
+#
